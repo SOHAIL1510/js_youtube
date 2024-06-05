090dcc236765241b01a3f43b7db8e39a5e91f5d1
SOHAIL1510 <148781579+SOHAIL1510@users.noreply.github.com>
2024-06-05 11:16:39 +0000

    variable

---
+++ variables.js
@@ -0,0 +1,12 @@
+let AccountEmail = "soha98@gmail.com"
+var AccountCity = "nashik"
+let AccountState;
+AccountPass="12345"
+console.log("sohail")
+
+AccountEmail="kmdkdf@jd.com"
+AccountCity="mumbai"
+AccountPass="7654"
+console.log("sohail")
+
+console.log(AccountCity);
