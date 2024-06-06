7645e5daee6554617798531432ced38c4518a744
SOHAIL1510 <148781579+SOHAIL1510@users.noreply.github.com>
2024-06-06 07:09:50 +0000

    var

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+let AccountEmail = "soha98@gmail.com"
+var AccountCity = "nashik"
+let AccountState;
+AccountPass="12345"
+
+AccountEmail="kmdkdf@jd.com"
+AccountCity="mumbai"
+AccountPass="7654"
+
+console.log(AccountCity);
+console.table([AccountEmail,AccountCity,AccountPass])
+//var is not preffered mostly as it is having issue in block scope and functional scope
+//let and const are the block scope while var is global scope 
+//all these 3 are the different keywords used in js to declare variables
