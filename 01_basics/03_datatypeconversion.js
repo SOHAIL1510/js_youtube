ed911c2feb0d8bb516d2741ad8e5d8144fcae7be
SOHAIL1510 <148781579+SOHAIL1510@users.noreply.github.com>
2024-06-13 07:05:58 +0000

    conversion of datatypes

---
+++ 01_basics/03_datatypeconversion.js
@@ -0,0 +1,26 @@
+let score = "33abc"
+
+console.log(typeof score)
+
+let stringnumber = Number(score)
+console.log(stringnumber)
+console.log(typeof stringnumber)
+
+//like wise you can convert data from number to string and vice versa 
+//if you go with 33=>number
+//"33abc"=>nan
+//true=>1
+//false=>0
+
+
+let isloggedin = 0
+let booleanisloggedin = Boolean(isloggedin)
+console.log(isloggedin)
+console.log(typeof isloggedin)
+console.log(booleanisloggedin)
+console.log(typeof booleanisloggedin)
+
+//1=>true
+//0=>false
+//string=>true
+//""=>false
