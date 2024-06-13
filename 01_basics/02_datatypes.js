ae8163c378f6619b9d24157a11a3fe0a7254e2de
SOHAIL1510 <148781579+SOHAIL1510@users.noreply.github.com>
2024-06-13 06:35:52 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict"  // code will work as newer syntax
+//alert(3+3); // we are working with node js not with browser
+// number=> 2 to power 53
+// bigint=> for bigger integer not in our case but for the biger data
+// string=""
+//boolean=> true/false
+//null=>standalone value
+// undefined=>
+//symbol=> unique
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);// object
+
