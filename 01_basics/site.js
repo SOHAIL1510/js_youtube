39626da476992c8ffe5642b9d19907acb6984716
SOHAIL1510 <148781579+SOHAIL1510@users.noreply.github.com>
2024-06-22 07:41:14 +0000

    site

---
+++ 01_basics/site.js
@@ -0,0 +1,19 @@
+const product = {
+    name : "ball Pen",
+    rating : 3,
+    offer : 2,
+    price : 300,
+
+};
+console.log(product)
+
+const profile = {
+    username : "sohail",
+    isfollow : false , 
+    followers : 500,
+    following : 300 , 
+    ismessage : true,
+
+
+};
+console.log(typeof profile)
