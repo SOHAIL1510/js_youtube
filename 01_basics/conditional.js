9b0a7edb6f36e9f74d0d619b4e17f6342d943f61
SOHAIL1510 <148781579+SOHAIL1510@users.noreply.github.com>
2024-06-22 07:40:27 +0000

    conditional statement

---
+++ 01_basics/conditional.js
@@ -0,0 +1,37 @@
+// if statement 
+let age = 25;
+if (age >= 18)
+  console.log("you can vote because you are", age)
+
+
+//if else statement 
+let colour_mode = "green"
+if (colour_mode=="dark")
+    {
+    console.log("black")
+}
+
+else{
+
+ console.log("white")
+}
+
+
+//else if statement 
+let mode = "dark"
+let colour ;
+if (mode==="dark"){
+    console.log("black")
+}
+else if(mode==="blue"){
+    console.log("sky blue")
+}
+else if (mode==="red"){
+    console.log("bloodshed")
+}
+else {
+    console.log("white")
+}
+console.log(colour)
+
+
