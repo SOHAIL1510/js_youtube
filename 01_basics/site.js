f922f254a52e8f1628aa13f1345239eb8dafd989
SOHAIL1510 <148781579+SOHAIL1510@users.noreply.github.com>
2024-06-23 05:38:41 +0000

    practice ques

--- 01_basics/site.js
+++ 01_basics/site.js
@@ -18,3 +18,33 @@ const profile = {
 };
 console.log(typeof profile)
 
+
+
+
+
+
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+rl.question('Enter Your Name: ', (input) => {
+    console.log(`Your Name is : ${input}`);
+    rl.close();
+});
+//const score = prompt ("enter your score")
+//console.log(score)
+let score = 100
+
+if (score>=90 && score<=100)
+    console.log("grade A")
+else if (score<=89 && score>=70)
+    console.log("grade B")
+else if (score<=69 && score>=40)
+    console.log("grade C")
+else if (score > 100)
+    console.log("invalid score")
+else {
+    console.log("fail")
+}
